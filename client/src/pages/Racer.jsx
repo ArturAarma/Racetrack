1ae4faa8fdba83a778baf478d4d386005f0ba45f
arturaarma <artur.aarma@gmail.com>
2025-03-04 18:15:57 +0200

    Added a feature that makes drivers active upon selecting them, using socket io

--- client/src/pages/Racer.jsx
+++ client/src/pages/Racer.jsx
@@ -1,19 +1,43 @@
 import "./Racer.css";
-import React from 'react';
 import { Link } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import React, { useContext, useState, useEffect } from "react";
+import RacerList from "../components/RacerList.ts";
+import { SocketContext } from "../context/SocketContext";
 
 
 
 const Racer = () => {
+
+    const socket = useContext(SocketContext);
+    const [driver, setDriver] = useState(null);
+  
+    useEffect(() => {
+      if (!socket) return;
+  
+      const handleActiveDriver = (selectedDriver) => {
+        setDriver(selectedDriver);
+      };
+  
+      socket.on("getDriver", handleActiveDriver);
+  
+      return () => {
+        socket.off("getDriver", handleActiveDriver);
+      };
+    }, [socket]);
+  
     return (
-        <div>
-            <div>You are on the Racer homepage</div>
-            <Link to="/" className="bbutton">Back to the main page</Link>
-        
-        </div>
-    )
-    
+        <div className="RacerSelection">
+        {driver ? ( 
+            <div className="driverInfo">
+                <div>Active driver: {driver.name}</div>
+                
+            </div>
+        ) : (
+            <div>No active driver selected.</div>
+        )}
+    </div>
+);
 };
 
 export default Racer;
