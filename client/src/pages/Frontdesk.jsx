ba2402d43d251c21ec664f722ac633b0a341832e
arturaarma <artur.aarma@gmail.com>
2025-03-04 18:15:57 +0200

    Added a feature that makes drivers active upon selecting them, using socket io

--- client/src/pages/Frontdesk.jsx
+++ client/src/pages/Frontdesk.jsx
@@ -1,52 +1,49 @@
-import "./frontdesk.css";
-import React, { useState } from 'react';
+import "./Racer.css";
 import { Link } from 'react-router-dom';
-import AddRacer from "../components/AddRacer";
+import ReactDOM from 'react-dom/client';
+import React, { useContext, useState, useEffect } from "react";
 import RacerList from "../components/RacerList.ts";
+import { SocketContext } from "../context/SocketContext";
 
-function Frontdesk() {
-    const [drivers, setDrivers] = useState([]); // State to store the list of divs
 
-    const addDriver = () => {
-        // Add a new div to the list
-        if (drivers.length < RacerList.length) {
-            const newDriver = {
-                id: RacerList[drivers.length].name, 
-                name: RacerList[drivers.length].name,
-            };
-            setDrivers([...drivers, newDriver])
+
+const Frontdesk = () => {
+
+    const socket = useContext(SocketContext);
+    const [selectedDriver, setSelectedDriver] = useState(null);
+
+    useEffect(() => {
+        if (socket) {
+            socket.emit("makeDriverActive", selectedDriver);
         }
-    };
-
-    function removeThisDriver(id) {
-        setDrivers(drivers.filter(driver => driver.id !== id));
-      }
-
+    }, [socket, selectedDriver]);
     
+
+
     return (
-        <div className="container">
-            <div className="frontdeskHeader">Front desk</div>
-            <div className="racerpanel">
-                <div className="PanelButtons">
-                    
-                    <AddRacer addDriver={addDriver} />
-                </div>
-                <div className="Racers">
-                {drivers.map(driver => (
+        <div>
+            <div>You are on the Racer homepage</div>
+            <Link to="/" className="bbutton">Back to the main page</Link>
+            
+                <div className="RacerSelection">
+                    {RacerList.map(driver => (
                         <div key={driver.id} className="drivers" id={driver.id}>
                             <div>{driver.name}</div>
-                            <button className="removebutton" onClick={() => removeThisDriver(driver.id)}>
-                                X
+                            <button className="removebutton" onClick={() => setSelectedDriver(driver)}>
+                                +
                             </button>
+                           
                         </div>
                     ))}
                 </div>
-            </div>
-            <div>
-                <Link to="/" className="bbutton" id="linkback">Back to the main page</Link>
-            </div>
+                <div className="ActiveRacers">
+                {selectedDriver ? <div>Active Driver: {selectedDriver.name}</div> : "No driver selected"}
+                </div>
+            
+        
         </div>
-    );
-}
+    )
+    
+};
 
 export default Frontdesk;
