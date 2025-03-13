6c305608a3cc228cb207353f8c889a56b40041a5
MihkelF <6264359+MihkelF@users.noreply.github.com>
2025-03-13 15:31:46 +1000

    improved front-desk css, cleaned code

--- client/src/pages/FrontDesk2.jsx
+++ client/src/pages/FrontDesk2.jsx
@@ -22,6 +22,7 @@ function Frontdesk() {
             drivers: ["", "", "", "", "", "", "", ""],
             isActive: false,
             isFinished: false,
+            raceMode: "",
           },
         ]);
       }
@@ -135,85 +136,37 @@ function EditSession({ session, onRemove, onUpdate, queue }) {
 
   return (
     <div className="session-container">
-      <div className="session-header">Session name: {session.name}</div>
-      <div className="session-header">Place in line: {queue}</div>
+      <div className="session-header">
+        Session #{queue}: <br></br>
+        {session.name}
+      </div>
       <div className={session.isActive ? "session-active" : "session-inactive"}>
         {session.isActive
           ? "Session active, unable to edit"
           : "Session inactive"}
       </div>
+
       <div className="session-drivers">
         <form onSubmit={handleSubmit}>
-          <label>Car #1 </label>
-          <input
-            type="text"
-            placeholder="Driver 1"
-            value={drivers[0] || ""}
-            onChange={(e) => handleChange(0, e.target.value)}
-            disabled={!editEnabled}
-          />
-          <label>Car #2 </label>
-          <input
-            type="text"
-            placeholder="Driver 2"
-            value={drivers[1] || ""}
-            onChange={(e) => handleChange(1, e.target.value)}
-            disabled={!editEnabled}
-          />
-          <label>Car #3 </label>
-          <input
-            type="text"
-            placeholder="Driver 3"
-            value={drivers[2] || ""}
-            onChange={(e) => handleChange(2, e.target.value)}
-            disabled={!editEnabled}
-          />
-          <label>Car #4 </label>
-          <input
-            type="text"
-            placeholder="Driver 4"
-            value={drivers[3] || ""}
-            onChange={(e) => handleChange(3, e.target.value)}
-            disabled={!editEnabled}
-          />
-          <label>Car #5 </label>
-          <input
-            type="text"
-            placeholder="Driver 5"
-            value={drivers[4] || ""}
-            onChange={(e) => handleChange(4, e.target.value)}
-            disabled={!editEnabled}
-          />
-          <label>Car #6 </label>
-          <input
-            type="text"
-            placeholder="Driver 6"
-            value={drivers[5] || ""}
-            onChange={(e) => handleChange(5, e.target.value)}
-            disabled={!editEnabled}
-          />
-          <label>Car #7 </label>
-          <input
-            type="text"
-            placeholder="Driver 7"
-            value={drivers[6] || ""}
-            onChange={(e) => handleChange(6, e.target.value)}
-            disabled={!editEnabled}
-          />
-          <label>Car #8 </label>
-          <input
-            type="text"
-            placeholder="Driver 8"
-            value={drivers[7] || ""}
-            onChange={(e) => handleChange(7, e.target.value)}
-            disabled={!editEnabled}
-          />
+          {drivers.map((driver, index) => (
+            <div key={index} className="driver-row">
+              <label>Car #{index + 1} </label>
+              <input
+                type="text"
+                placeholder={`Driver ${index + 1}`}
+                value={drivers[index] || ""}
+                onChange={(e) => handleChange(index, e.target.value)}
+                disabled={!editEnabled}
+              />
+            </div>
+          ))}
           {editEnabled && (
             <button type="submit" className="edit-button">
               Confirm
             </button>
           )}
         </form>
+
         {!editEnabled && !session.isActive && (
           <button className="edit-button" onClick={toggleEdit}>
             Edit
