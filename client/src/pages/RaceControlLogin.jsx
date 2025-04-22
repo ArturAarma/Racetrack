875a0df464dd7e7029f59923c20807b9738091c4
MihkelF <6264359+MihkelF@users.noreply.github.com>
2025-04-23 06:10:27 +1000

    save changes

--- client/src/pages/RaceControlLogin.jsx
+++ client/src/pages/RaceControlLogin.jsx
@@ -1,65 +1,64 @@
 import "./RaceControlLogin.css";
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import RaceControl from "./RaceControl";
 
-import checkPw from '../components/CheckPw';
+import checkPw from "../components/CheckPw";
 
 function RaceControlLogin() {
-   
-   
-    const [password, setPassword] = useState("");
-    const [loginStatus, setLoginStatus] = useState(null);
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState(null);
 
-    const loginClick = (event) => {
-        event.preventDefault(); 
-        const status = checkPw(password);
-        setLoginStatus(status.toLowerCase());
-    };
-    
+  const loginClick = (event) => {
+    event.preventDefault();
+    const status = checkPw(password);
+    setLoginStatus(status.toLowerCase());
+  };
 
-
-    return (
-        <div className="container">
-            <form onSubmit={loginClick}>
-            {loginStatus === null ? (
-                <div>
-                <div className="login">
-                    <input 
-                    type="text" 
-                    placeholder="password" 
-                    id="loginInput" 
-                    defaultValue={""} 
-                    onChange={e => setPassword(e.target.value)} 
-                    />
-                    <button type="submit">Login</button>
-                </div>
-                <div>
-                    <Link to="/" className="bbutton">Back to the main page</Link>
-                </div>
-                </div>
-            ) : loginStatus === "security" ? (
-                <div className='racecontrol'>
-                   <RaceControl/>
-                </div>
-            ) : loginStatus === "invalid" ? (
-                <div className='invalid'>
-                    <div>Enter valid password</div>
-                    <Link to="/" className="bbutton">Back to the main page</Link>
-                </div>
-            ) : (
-                <div className='invalid'>
-                    <div>Unexpected error. Please try again.</div>
-                    <Link to="/" className="bbutton">Back to the main page</Link>
-                </div>
-
-            )}
-            </form>
-        </div>
-    );
-    
+  return (
+    <div className="container">
+      <form onSubmit={loginClick}>
+        {loginStatus === null ? (
+          <div>
+            <div className="login">
+              <input
+                type="text"
+                placeholder="password"
+                id="loginInput"
+                defaultValue={""}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit">Login</button>
+            </div>
+            <div>
+              <Link to="/" className="bbutton">
+                Back to the main page
+              </Link>
+            </div>
+          </div>
+        ) : loginStatus === "security" ? (
+          <div className="racecontrol">
+            <RaceControl />
+          </div>
+        ) : loginStatus === "invalid" ? (
+          <div className="invalid">
+            <div>Enter valid password</div>
+            <Link to="/" className="bbutton">
+              Back to the main page
+            </Link>
+          </div>
+        ) : (
+          <div className="invalid">
+            <div>Unexpected error. Please try again.</div>
+            <Link to="/" className="bbutton">
+              Back to the main page
+            </Link>
+          </div>
+        )}
+      </form>
+    </div>
+  );
 }
 
 export default RaceControlLogin;
-
