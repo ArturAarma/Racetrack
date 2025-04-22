25f74c2bb6e9a879364268cd9597abbc9e01ba87
arturaarma <artur.aarma@gmail.com>
2025-04-23 02:48:45 +0300

    Quick fix

--- client/src/pages/NextRace.jsx
+++ client/src/pages/NextRace.jsx
@@ -6,75 +6,75 @@ import { SocketContext } from "../context/SocketContext";
 
 function NextRace() {
 
-    const socket = useContext(SocketContext);
-    
-    const [sessions, setSessions] = useState([]);
+  const socket = useContext(SocketContext);
 
-    useEffect(() => {
-        if (!socket) return;
-    
-        socket.on("getSessions", (updatedCurrentSession) => {
-          setSessions(updatedCurrentSession);
-        });
-    
-        socket.on("startedRaceAlert", () => {
-          socket.emit("requestSessions");
-        });
+  const [sessions, setSessions] = useState([]);
 
-        socket.on("sessionHasEnded", () => {
-          socket.emit("requestSessions");
-        });
-        
-        
-        
-        
-    
-        return () => {
-          socket.off("getSessions");
-          socket.off("startedRaceAlert");
-          socket.off("sessionsHasEnded");
-        };
-      }, [socket]);
-    
-    
-    return (
-        <div className="container">
-            <div className="frontdeskHeader">Front desk</div>
-            <div className="racerpanel">
-              <div className="nextsession">
-                <h1>Next session</h1>
-                <div className="sessioninfo">
-                    {sessions.length > 0 ? (
-                        <div className="sessiondetails">
-                            <p>Up next: {sessions[0].name}</p>
-                            
-                            <div className="leader-board-container">
-      
-                            {sessions &&
-          sessions[0].drivers.map((driver, index) => (
-            <div className="lb-position" key={index}>
-              {driver.name} 
-              
-            </div>
-            
-          ))}
-          <div>
-            <p>Proceed to the paddock</p>
-          </div>
-      </div>
-      
-                        </div>
-                    ) : (
-                        <p>No sessions available</p>
-                    )}
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on("getSessions", (updatedCurrentSession) => {
+      setSessions(updatedCurrentSession);
+    });
+
+    socket.on("startedRaceAlert", () => {
+      socket.emit("requestSessions");
+    });
+
+    socket.on("sessionHasEnded", () => {
+      socket.emit("requestSessions");
+    });
+
+
+
+
+
+    return () => {
+      socket.off("getSessions");
+      socket.off("startedRaceAlert");
+      socket.off("sessionsHasEnded");
+    };
+  }, [socket]);
+
+
+  return (
+    <div className="container">
+      <div className="frontdeskHeader">Next Race</div>
+      <div className="racerpanel">
+        <div className="nextsession">
+          <h1>Next session</h1>
+          <div className="sessioninfo">
+            {sessions.length > 0 ? (
+              <div className="sessiondetails">
+                <p>Up next: {sessions[0].name}</p>
+
+                <div className="leader-board-container">
+
+                  {sessions &&
+                    sessions[0].drivers.map((driver, index) => (
+                      <div className="lb-position" key={index}>
+                        {driver.name}
+
+                      </div>
+
+                    ))}
+                  <div>
+                    <p>Proceed to the paddock</p>
+                  </div>
                 </div>
 
               </div>
-                
-                <Link to="/" className="bbutton" id="linkback">Back to the main page</Link>
-            </div>
+            ) : (
+              <p>No sessions available</p>
+            )}
+          </div>
+
         </div>
-    );
+
+        <Link to="/" className="bbutton" id="linkback">Back to the main page</Link>
+      </div>
+    </div>
+  );
 }
 
 export default NextRace;
